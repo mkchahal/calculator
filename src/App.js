5b192c4df12858aa696d2bb267658d77ad970f58
Mandeep Kaur Chahal <99150991+mkchahal@users.noreply.github.com>
2022-05-30 23:17:50 -0700

    Imported the components to App.jsx and set the initial values.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import Wrapper from "./components/Wrapper/Wrapper";
+import Screen from "./components/Screen/Screen";
+import ButtonBox from "./components/ButtonBox/ButtonBox";
+import Button from "./components/Button/Button";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        <Button
+          className=""
+          value="0"
+          onClick={() => {
+            console.log("Button clicked!")
+          }}  
+        />
+      </ButtonBox> 
+    </Wrapper>
   );
 }
 
