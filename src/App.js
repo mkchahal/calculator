f3ee5483c24d3e6cf7d8038c9473cdff436eda2b
Mandeep Kaur Chahal <99150991+mkchahal@users.noreply.github.com>
2022-06-06 23:40:56 -0700

    Added all the button functions handler to the App.js.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import ButtonBox from "./components/ButtonBox/ButtonBox";
 import Button from "./components/Button/Button";
 import { useState } from "react";
 
+const btnValues = [
+  ["AC", "+/-", "%", "÷"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
 function App() {
 
   let [calc, setCalc] = useState({
@@ -12,13 +20,98 @@ function App() {
     res: 0
   });
 
-  const btnValues = [
-      ["AC", "+/-", "%", "÷"],
-      [7, 8, 9, "X"],
-      [4, 5, 6, "-"],
-      [1, 2, 3, "+"],
-      [0, ".", "="],
-    ];
+  const numClickHandler = (event) => {
+    event.preventDefault();
+    const value = event.target.innerHTML;
+
+    if(calc.num.length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? "0"
+            : calc.num % 1 === 0 
+            ? Number(calc.num + value)
+            : calc.num + value,
+        res: !calc.sign ? 0 : calc.res
+      })
+    }
+  }
+
+  const commaClickHandler = (event) => {
+    event.preventDefault();
+    const value = event.target.innerHTML;
+
+    setCalc({
+      ...calc, 
+      num: calc.num.toString().includes(".") ? calc.num : calc.num + value
+    })
+  }
+
+  const signClickHandler = (event) => {
+    event.preventDefault();
+    const value = event.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,  
+      num: 0
+    })
+  }
+
+  const equalsClickHandler = () => {
+    if (calc.sign && calc.num) {
+      const math = (a, b, sign) => 
+        sign === "+"
+          ? a + b
+          : sign === "-"
+          ? a - b
+          : sign === "X"
+          ? a * b
+          : a / b;
+
+      setCalc({
+        ...calc, 
+        res:
+          calc.num === "0" && calc.sign === "/"
+            ? "Cannot divide with zero"
+            : math(Number(calc.res), Number(calc.num), calc.sign), 
+        sign: "", 
+        num: 0
+      })
+    }
+  }
+
+  const invertClickHandler = () => {
+    setCalc({
+      ...calc, 
+      num: calc.num ? calc.num * -1 : 0,
+      res: calc.res ? calc.res * -1 : 0,
+      sign: ""
+    })
+  }
+
+  const percentClickHandler = () => {
+    let num = calc.num ? parseFloat(calc.num) : 0;
+    let res = calc.res ? parseFloat(calc.res) : 0;
+
+    setCalc({
+      ...calc, 
+      num: (num /= 100),
+      res: (res /= 100),
+      sign: ""
+    })
+  }
+
+  const resetClickHandler = () => {
+    setCalc({
+      ...calc, 
+      sign: "", 
+      num: 0,
+      res: 0
+    })
+  }
 
   return (
     <Wrapper>
@@ -47,9 +140,7 @@ function App() {
                     : numClickHandler
                 }
               />
-            )
-          })
-        }
+            )})}
       </ButtonBox> 
     </Wrapper>
   );
